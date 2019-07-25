82e374abcd8105082845cf889e955603a749d7d4
CJFJack <398741302@qq.com>
2019-07-25 16:03:18 +0800

    learn1

---
+++ learn1.py
@@ -0,0 +1,70 @@
+# -*- encoding: utf-8 -*-
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import time
+
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['font.sans-serif'] = 'SimHei'
+mpl.rcParams['axes.unicode_minus'] = False
+
+path1 = r'datas/household_power_consumption_10000.txt'
+df = pd.read_csv(path1, sep=';', low_memory=False)
+# print(type(df))
+# print(df.index)
+# print(df.columns)
+# print(df.head(3))
+# print(df.info())
+
+# 异常数据的处理
+new_df = df.replace('?', np.nan)  # 将 ? 的数据替换成 nan
+datas = new_df.dropna(axis=0, how='any')  # 每一行数据中，如果存在一个特征值为nan，则整行删除
+
+# print(datas.index)
+# print(datas.columns)
+# print(datas.describe().T)
+
+# print(datas)
+Y = datas['Global_active_power']
+# print(type(Y))
+print(Y.head(4))
+
+
+def data_format(dt):
+    # dt是Series [Date] [Time]
+    t = time.strptime(' '.join(dt), '%d/%m/%Y %H:%M:%S')
+    return (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
+
+
+X = datas.iloc[:, 0:2]
+X = X.apply(lambda x: pd.Series(data_format(x)), axis=1)
+# print(type(X))
+print(X.head(4))
+
+
+# 对数据进行测试集、训练集划分
+# X: 特征矩阵(类型一般是DataFrame)
+# Y: 特征对应的Label标签或目标属性(类型一般是Series)
+
+# test_size=0.2表示将数据集的百分之20%划分为测试集
+# 如果test_size和train_size都不设置，则默认划分25%为测试集，剩下为训练集
+# random_state=0表示随机种子等于0，默认是随机数，设置随机种子为了保证每次运行程序划分的训练集和测试集结果保持一样
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+# print(X_train.shape)
+# print(Y_test.shape)
+
+# # 随机种子的作用
+# np.random.seed(7)
+# arr1 = np.random.randint(1, 11, (10, ))
+# print(arr1)
+# np.random.seed(7)
+# arr2 = np.random.randint(1, 11, (10, ))
+# print(arr2)
+
+print(X_train.describe().T)
