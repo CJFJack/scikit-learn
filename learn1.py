dab27a72c5819b49142ffac2e6273a8d51c9e8f3
CJFJack <398741302@qq.com>
2019-07-25 23:07:07 +0800

    scikit learn linear regerssion

--- learn1.py
+++ learn1.py
@@ -14,7 +14,7 @@ mpl.rcParams['font.family'] = 'sans-serif'
 mpl.rcParams['font.sans-serif'] = 'SimHei'
 mpl.rcParams['axes.unicode_minus'] = False
 
-path1 = r'datas/household_power_consumption_10000.txt'
+path1 = r'datas/household_power_consumption .txt'
 df = pd.read_csv(path1, sep=';', low_memory=False)
 # print(type(df))
 # print(df.index)
@@ -47,7 +47,6 @@ X = X.apply(lambda x: pd.Series(data_format(x)), axis=1)
 # print(type(X))
 print(X.head(4))
 
-
 # 对数据进行测试集、训练集划分
 # X: 特征矩阵(类型一般是DataFrame)
 # Y: 特征对应的Label标签或目标属性(类型一般是Series)
@@ -70,5 +69,40 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 print(X_train.describe().T)
 
 ss = StandardScaler()
+# 训练加转化
 X_train = ss.fit_transform(X_train)
-X_test = ss.fit_transform(X_test)
+# 转化
+X_test = ss.transform(X_test)
+# print(type(X_train))
+# print(pd.DataFrame(X_train).describe().T)
+
+# fit_intercept=True表示特征系数w0
+# normalize=False表示不使用内置的数据标准化模块
+# copy_X=True表示不改变原数据
+# n_jobs表示并行任务数
+lr = LinearRegression(fit_intercept=True)
+lr.fit(X_train, Y_train)
+y_predict = lr.predict(X_test)
+
+# R^2代表预测的准确率
+print('训练集上的R^2：', lr.score(X_train, Y_train))
+print('训练集上的R^2：', lr.score(X_test, Y_test))
+
+# mse = np.average((y_predict - Y_test) ** 2)
+# rmse = np.sqrt(mse)
+# print('rmse: ', rmse)
+
+print('模型训练后的系数：', end='')
+print(lr.coef_)
+
+print('模型的截距:', lr.intercept_)
+
+t = np.arange(len(X_test))
+plt.figure(facecolor='w')
+plt.plot(t, Y_test, 'r--', linewidth=2, label='真实值')
+plt.plot(t, y_predict, 'g--', linewidth=2, label='预测值')
+
+plt.legend(loc='upper left')
+plt.title('线性回归预测时间和功率之间的关系', fontsize=20)
+plt.grid(b=True)
+plt.show()
