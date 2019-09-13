bbcad033ad0d721a19373a76f14499707c2f6721
CJFJack <398741302@qq.com>
2019-09-14 00:20:49 +0800

    knn

--- KNN/knn1.py
+++ KNN/knn1.py
@@ -1,4 +1,7 @@
 # -*- encoding: utf-8 -*-
+"""
+利用scikit-learn框架的KNN算法进行分类
+"""
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,11 +11,9 @@ import numpy as np
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
-print (X, y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2003)
 clf = KNeighborsClassifier(n_neighbors=3)
 clf.fit(X_train, y_train)
-correct = np.count_nonzero((clf.predict(X_test) == y_test) is True)
-
-print ("Accuracy is: %.3f" % (correct / len(X_test)))
+correct = np.count_nonzero((clf.predict(X_test) == y_test))
+print("Accuracy is: %.3f" % (correct / len(X_test)))
