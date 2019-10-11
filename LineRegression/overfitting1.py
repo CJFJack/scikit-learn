ee049a732ba51274d76f44a4b21915e6a5f3d12c
CJFJack <398741302@qq.com>
2019-10-11 22:24:50 +0800

    over fitting

--- LineRegression/overfitting1.py
+++ LineRegression/overfitting1.py
@@ -52,6 +52,115 @@ models = [
     ]),
     Pipeline([
         ('Poly', PolynomialFeatures(include_bias=False)),
-        ('Linear', ElasticNetCV(alphas=np.logspace(0, 1, 10), l1_ratio=[.1, .5, .7, .9, .95, 1]))
+        # l1_ratio: 给定EN算法中L1正则项在整个惩罚项中的比例，这里给定一个列表；
+        # l1_ratio: 也就是ppt中的p，p的范围是[0, 1]
+        # alphas也就是ppt中的兰木达
+        # alphas表示的是在CV交叉验证的过程中，EN算法L!正则项的权重比例的可选值的范围
+        ('Linear', ElasticNetCV(alphas=np.logspace(0, 1, 10), l1_ratio=[.1, .5, .7, .9, .95, 1], fit_intercept=False))
     ]),
 ]
+
+# 线性模型过拟合图形识别
+plt.figure(facecolor='w')
+degree = np.arange(1, N, 4)  # 阶
+dm = degree.size
+colors = []  # 颜色
+for c in np.linspace(16711680, 255, dm):
+    colors.append('#%06x' % int(c))
+
+model = models[0]
+for i, d in enumerate(degree):
+    plt.subplot(int(np.ceil(dm / 2.0)), 2, i + 1)
+    plt.plot(x, y, 'ro', ms=10, zorder=N)
+
+    # 设置阶数
+    model.set_params(Poly__degree=d)
+    # 模型训练
+    model.fit(x, y.ravel())
+
+    lin = model.get_params('Linear')['Linear']
+    output = u'%d阶，系数为：' % (d)
+    # 判断lin对象中是否有对应的属性
+    if hasattr(lin, 'alpha_'):
+        idx = output.find(u'系数')
+        output = output[:idx] + (u'alpha=%.6f, ' % lin.alpha_) + output[idx:]
+    if hasattr(lin, 'l1_ratio_'):
+        idx = output.find(u'系数')
+        output = output[:idx] + (u'l1_radio=%.6f, ' % lin.l1_radio_) + output[idx:]
+    x_hat = np.linspace(x.min(), x.max(), num=100)  # 产生模拟数据
+    x_hat.shape = -1, 1
+    y_hat = model.predict(x_hat)
+    s = model.score(x, y)
+
+    z = N - 1 if (d == 2) else 0
+    label = u'%d阶，正确率=%.3f' % (d, s)
+    plt.plot(x_hat, y_hat, color=colors[i], lw=2, alpha=0.75, label=label, zorder=z)
+
+    plt.legend(loc='upper left')
+    plt.grid(True)
+    plt.xlabel('X', fontsize=16)
+    plt.xlabel('Y', fontsize=16)
+
+plt.tight_layout(1, rect=(0, 0, 1, 0.95))
+plt.suptitle(u'线性回归过拟合显示', fontsize=22)
+plt.show()
+
+# 线性回归，Lasso回归，Ridge回归，ElasticNet比较
+plt.figure(facecolor='w')
+degree = np.arange(1, N, 2)
+dm = degree.size
+colors = []  # 颜色
+for c in np.linspace(16711680, 255, dm):
+    colors.append('#%06x' % int(c))
+titles = [u'线性回归', u'Ridge回归', u'Lasso回归', u'ElasticNet']
+
+for t in range(4):
+    model = models[t]  # 选择了模型--具体的pipeline(线性、Lasso、Ridge、EN)
+    plt.subplot(2, 2, t + 1)  # 选择具体的子图
+    plt.plot(x, y, 'ro', ms=10, zorder=N)  # 在子图中画原始数据点； zorder：图像显示在第N个位置
+
+    # 遍历不同的多项式的阶，看不同阶的情况下，模型的效果
+    for i, d in enumerate(degree):
+        # 设置阶数（多项式）
+        model.set_params(Poly__degree=d)
+        # 模型训练
+        model.fit(x, y.ravel())
+
+        # 获取得到具体的算法模型
+        # model.get_params()方法返回的其实是一个dict对象，后面的Linear其实是dict对象的key
+        # 也是我们在定义Pipeline的时候给定的一个名称值
+        lin = model.get_params()['Linear']
+        # 打印数据
+        output = u'%s:%d阶，系数为：' % (titles[t], d)
+        # 判断lin对象中是否有对应的属性
+        if hasattr(lin, 'alpha_'):  # 判断lin这个模型中是否有alpha这个属性
+            idx = output.find(u'系数')
+            output = output[:idx] + (u'alpha=%.6f, ' % lin.alpha_) + output[idx:]
+        if hasattr(lin, 'l1_radio'):  # 判断lin这个模型中是否有l1_radio这个属性
+            idx = output.find(u'系数')
+            output = output[:idx] + (u'l1_radio=%.6f, ' % lin.l1_radio) + output[idx:]
+        # line.coef_: 获取线性模型的参数列表，也就是我们ppt中的theta值，ravel()将结果转换为行列式
+        print(output, lin.coef_ravel())
+
+        # 产生模拟数据
+        x_hat = np.linspace(x.min(), x.max(), num=100)
+        x_hat.shape = -1, 1
+        # 数据预测
+        y_hat = model.predict(x_hat)
+        # 计算准确率
+        s = model.score(x, y)
+
+        # 当d等于5的时候，设置为N-1层，其他设置为0层；将d=5得这条线凸显出来
+        Z = N + 1 if d == 5 else 0
+        label = u'%d阶，正确率%.3f' % (d, s)
+        plt.plot(x_hat, y_hat, color=colors[i], lw=2, alpha=0.75, label=label, zorder=z)
+
+    plt.legend(loc='upper left')
+    plt.grid(True)
+    plt.title(titles[t])
+    plt.xlabel('X', fontdict=16)
+    plt.ylabel('Y', fontdict=16)
+
+plt.tight_layout(1, rect=(0,0,1,0.95))
+plt.suptitle(u'各种不同线性回归过拟合显示', fontsize=22)
+plt.show()
